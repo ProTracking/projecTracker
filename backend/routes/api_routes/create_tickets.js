14d713583d58d3e5ca8796411cc5e0c5ea020436
watsotc1 <taylor.c.watson@vanderbilt.edu>
2021-10-19 17:54:04 -0500

    Fixed Known bug in Create Ticket

--- backend/routes/api_routes/create_tickets.js
+++ backend/routes/api_routes/create_tickets.js
@@ -5,20 +5,21 @@ const db = require('../../db/connection');
 
 // Create Ticket
 router.post('/ticket', ({body}, res) => {
-    const sql = "INSERT INTO ticket (subject, content, html, priority, user_id) VALUES (?,?,?,?,?)";
     const param = [body.subject, body.content, body.html, body.priority, body.user_id];
-    console.log(param);
-    console.log(sql)
-    db.query(sql, param, (err, result) => {
-        console.log(err);
+    const sql = 'INSERT INTO ticket (subject,content,html,priority,user_id) VALUES (' +
+                '\'' + body.subject + '\',' +
+                '\'' + body.content + '\',' +
+                '\'' + body.html + '\',' +
+                '\'' + body.priority + '\',' +
+                       (body.user_id).toString() + ')';
+
+    db.query(sql, (err, result) => {
         if(err) {
             res.status(400).json({
                 error:err.message
             });
             return;
         }
-        console.log(sql);
-        console.log(body);
         res.json({
             message: 'Success',
             data: body

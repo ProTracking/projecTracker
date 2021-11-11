f495f2c7c18420149b75a22db37fce30c52b0669
anaswardeh <anaswardeh@gmail.com>
2021-11-11 18:36:24 -0500

    PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const db = require('./db/connection');
 const api_routes = require('./routes/api_routes');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 
 const app = express(); // Instantiating Express Server

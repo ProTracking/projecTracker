f5c70415104eb032f070cedfb4dfdfcc48e9a35b
anaswardeh <anaswardeh@gmail.com>
2021-11-11 15:25:27 -0500

    db settings

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'protracking',
-    password: '',
+    user: 'nvuzlquqnxlfxg',
+    host: 'ec2-3-229-166-245.compute-1.amazonaws.com',
+    database: 'dciebt79kl0pe6',
+    password: '026f9855463e27f0e63b1fb5dfc3c23b0bd1913bf35e9da7a168ec1fbd5649f4',
     port: 5432,
 });
 
